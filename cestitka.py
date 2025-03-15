c419dfe95b90d45e084b6c3cf997531ec4a16251
SpajsiPolaznik <nemanja.so05@gmail.com>
2025-03-15 15:58:36 +0100

    mejdžr čejndž
    ništa važno

--- cestitka.py
+++ cestitka.py
@@ -1,7 +1,9 @@
 """
 Draga Lano, 
 Ako postoji algoritam za savršenog prijatelja, ti si ga hakovala!  Dokaz da smo bez tvoje kreativnosti izgubljeni su tvoji genijalni storiji i postovi (iako ih ponekad čekamo po dva mjeseca - ali vredi). Verovatno bismo bez tebe i dalje buljili u prazan ekran, ali ovaj tekst moramo da napišemo sami, ili ipak uz malu pomoc ChatGPT-ja.
-Iako naše prijateljstvo traje svega dve godine, sa tobom smo se smejali, plakali, ispijali kafe, kucali kodove do kasno u noć... Svaki trenutak u tvom drustvu je poseban – kada smo se smejali do suza (#toaletPapir #aluminijumskaFolija) ili  kada si bila rame za plakanje (#ripJupyter).  Za mnoge ljude koza je samo životinja, ali samo prave glave znaju šta (ko) je to zapravo (#andigona<-). Dok obične koze brinu o ispaši i traže najsočniju travu, ova polu koza ima potpuno drugačije prioritete – ona živi za brzu vožnju, precizne strategije i SF-25 bolid, kao i ti. Umesto da se kladiš u čips, sada ćeš moći da uplaćuješ tikete I nešto zaradiš, taman da platiš virtuelnu mašinu. Bez nje ne možeš da završiš projekat, a da FIFI i pfe prijatelji ne bankrotiraju. Majski, Anđela i Ivan nas čeakju da za neku godinu svi zajedno tračarimo u kancu (#jedvacekanje). Nadamo se da ćeš nam i tada praviti stikere, samo u saradničkom izdanju (#limitedEdition), tad ćemo imati pravi dijamantski status. 
+Iako naše prijateljstvo traje svega dve godine, sa tobom smo se smejali, plakali, ispijali kafe, kucali kodove do kasno u noć... Svaki trenutak u tvom drustvu je poseban – kada smo se smejali do suza (#toaletPapir #aluminijumskaFolija) ili  kada si bila rame za plakanje (#ripJupyter).
+Za mnoge ljude koza je samo životinja, ali samo prave glave znaju šta (#ko) je to zapravo (#andigona<-). Dok obične koze brinu o ispaši i traže najsočniju travu, ova pfolu koza ima potpuno drugačije prioritete – ona živi za brzu vožnju, precizne strategije i SF-25 bolid, kao i ti. Umesto da se kladiš u čips, sada ćeš moći da uplaćuješ tikete i nešto zaradiš, taman da platiš virtuelnu mašinu. Bez nje ne možeš da završiš projekat, a da FIFI i pfe prijatelji ne bankrotiraju. 
+Majski, Anđela i Ivan nas već čekaju da za neku godinu svi zajedno tračarimo u kancu (#jedvacekanje). Nadamo se da ćeš nam i tada praviti stikere, samo u saradničkom izdanju (#limitedEdition), tad ćemo imati pravi dijamantski status. 
 Čeka vrag u mraku
 Ko Pac ću da uradim
 Thug Life tattoo na stomaku
@@ -9,8 +11,9 @@ Sedmi sprat, mrak u stanu........
 Vadim asa iz rukava...
 Da nam budeš živa zdrava
 Nek ti svaki dan bude kao san 
-Srećan 18. rođendan!
+Srećan ti 18. rođendan!
 """
+
 
 from __future__ import print_function
 import winsound
