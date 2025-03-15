3be4a9950edb33823af73b54c4335f5c83cb0353
SpajsiPolaznik <nemanja.so05@gmail.com>
2025-03-15 15:48:44 +0100

    Create cestitka.py
    Jur velkom
    
    Hepi brzdej

---
+++ cestitka.py
@@ -0,0 +1,101 @@
+"""
+Draga Lano, 
+Ako postoji algoritam za savršenog prijatelja, ti si ga hakovala!  Dokaz da smo bez tvoje kreativnosti izgubljeni su tvoji genijalni storiji i postovi (iako ih ponekad čekamo po dva mjeseca - ali vredi). Verovatno bismo bez tebe i dalje buljili u prazan ekran, ali ovaj tekst moramo da napišemo sami, ili ipak uz malu pomoc ChatGPT-ja.
+Iako naše prijateljstvo traje svega dve godine, sa tobom smo se smejali, plakali, ispijali kafe, kucali kodove do kasno u noć... Svaki trenutak u tvom drustvu je poseban – kada smo se smejali do suza (#toaletPapir #aluminijumskaFolija) ili  kada si bila rame za plakanje (#ripJupyter).  Za mnoge ljude koza je samo životinja, ali samo prave glave znaju šta (ko) je to zapravo (#andigona<-). Dok obične koze brinu o ispaši i traže najsočniju travu, ova polu koza ima potpuno drugačije prioritete – ona živi za brzu vožnju, precizne strategije i SF-25 bolid, kao i ti. Umesto da se kladiš u čips, sada ćeš moći da uplaćuješ tikete I nešto zaradiš, taman da platiš virtuelnu mašinu. Bez nje ne možeš da završiš projekat, a da FIFI i pfe prijatelji ne bankrotiraju. Majski, Anđela i Ivan nas čeakju da za neku godinu svi zajedno tračarimo u kancu (#jedvacekanje). Nadamo se da ćeš nam i tada praviti stikere, samo u saradničkom izdanju (#limitedEdition), tad ćemo imati pravi dijamantski status. 
+Čeka vrag u mraku
+Ko Pac ću da uradim
+Thug Life tattoo na stomaku
+Sedmi sprat, mrak u stanu........
+Vadim asa iz rukava...
+Da nam budeš živa zdrava
+Nek ti svaki dan bude kao san 
+Srećan 18. rođendan!
+"""
+
+from __future__ import print_function
+import winsound
+import time
+import sys
+
+name="Lana"
+
+print('Ha', end='')
+winsound.Beep(264, 250)
+time.sleep(500/2000.0)
+print('ppy ', end='')
+winsound.Beep(264, 250)
+time.sleep(250/2000.0)
+print('birth', end='')
+winsound.Beep(297, 1000)
+time.sleep(250/2000.0)
+print('day ', end='')
+winsound.Beep(264, 1000)
+time.sleep(250/2000.0)
+print('to ', end='')
+winsound.Beep(352, 1000)
+time.sleep(250/2000.0)
+print('you')
+winsound.Beep(330, 2000)
+time.sleep(500/2000.0)
+
+print('Ha', end='')
+winsound.Beep(264, 250)
+time.sleep(500/2000.0)
+print('ppy ', end='')
+winsound.Beep(264, 250)
+time.sleep(250/2000.0)
+print('birth', end='')
+winsound.Beep(297, 1000)
+time.sleep(250/2000.0)
+print('day ', end='')
+winsound.Beep(264, 1000)
+time.sleep(250/2000.0)
+print('to ', end='')
+winsound.Beep(396, 1000)
+time.sleep(250/2000.0)
+print('you')
+winsound.Beep(352, 2000)
+time.sleep(500/2000.0)
+
+print('Ha', end='')
+winsound.Beep(264, 250)
+time.sleep(250/2000.0)
+print('ppy ', end='')
+winsound.Beep(264, 500)
+time.sleep(250/1000.0)
+print('birth', end='')
+winsound.Beep(440, 1000)
+time.sleep(250/2000.0)
+print('day ', end='')
+winsound.Beep(352, 1000)
+time.sleep(250/2000.0)
+print('dear ', end='')
+winsound.Beep(330, 1000)
+print(name)
+time.sleep(250/2000.0)
+winsound.Beep(297, 1000)
+
+winsound.Beep(440, 1000)
+time.sleep(250/2000.0)
+
+time.sleep(500/2000.0)
+print('Ha', end='')
+winsound.Beep(466, 250)
+time.sleep(500/2000.0)
+print('ppy ', end='')
+winsound.Beep(466, 250)
+time.sleep(250/2000.0)
+print('birth', end='')
+winsound.Beep(440, 1000)
+time.sleep(250/2000.0)
+print('day ', end='')
+winsound.Beep(352, 1000)
+time.sleep(250/2000.0)
+print('to ', end='')
+winsound.Beep(396, 1000)
+time.sleep(250/2000.0)
+print('you')
+winsound.Beep(352, 2000)
+time.sleep(250/2000.0)
+
+print('HAPPY BIRTHDAY ' + name + ' <3')
